337035d3c83baf188c59dcdab7c1a9efaaaa19b1
WLMaia <wilker.maia@ufac.br>
2022-04-24 12:03:09 -0500

    programas testes

---
+++ src/programa_04.cpp
@@ -0,0 +1,10 @@
+#define _USE_MATH_DEFINES // must include this!
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+  // M_PI = 3.14159265358979323846;
+  std::cout << M_PI << " " << M_E << " " << M_SQRT2 << std::endl;
+  return 0;
+}
